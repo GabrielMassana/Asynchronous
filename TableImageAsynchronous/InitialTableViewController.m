2ca304b91fb58dd00fd92d8c8bcc3eeb5d70615d
GabrielMassana <fosc2000@gmail.com>
2012-10-17 14:50:25 +0100

    funciona

---
+++ TableImageAsynchronous/InitialTableViewController.m
@@ -0,0 +1,117 @@
+//
+//  InitialTableViewController.m
+//  TableImageAsynchronous
+//
+//  Created by Jose Antonio Gabriel Massana on 16/10/12.
+//  Copyright (c) 2012 Jose Antonio Gabriel Massana. All rights reserved.
+//
+
+#import "InitialTableViewController.h"
+
+
+@implementation InitialTableViewController
+{
+    NSString *path;
+    NSDictionary *rootLevel;
+    NSString *imagePath;
+    NSArray *countries;
+    NSNumberFormatter * floatFormatter;
+    /////////////////////////
+    dispatch_queue_t mainQueue;
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    path = [[NSBundle mainBundle] pathForResource:@"Countries" ofType:@"plist"];
+    rootLevel = [[NSDictionary alloc] initWithContentsOfFile:path];
+    countries = [rootLevel objectForKey:@"Countries"];
+    imagePath = [rootLevel objectForKey:@"ImagePath"];
+    
+    
+    /////////////////////////
+   // mainQueue = dispatch_get_main_queue();
+
+    mainQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [countries count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    // Configure the cell...
+    
+    UILabel *label = (UILabel *)[cell viewWithTag:200];
+    UIImageView *imageFlag = (UIImageView *)[cell viewWithTag:300];
+    
+    label.text = [[countries objectAtIndex:indexPath.row] objectForKey:@"Name"];
+    
+    imageFlag.image = [UIImage imageNamed:@"Placeholder.png"];
+    
+    dispatch_async(mainQueue, ^(void) {
+    NSString *url = [NSString stringWithFormat:@"%@%@", imagePath, [[countries objectAtIndex:indexPath.row] objectForKey:@"Flag"]];
+    NSData* imageData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:url]];
+    UIImage* image = [[UIImage alloc] initWithData:imageData];
+    
+    imageFlag.image = image;
+        
+    NSLog(@" Downloading Image...: %@", [[countries objectAtIndex:indexPath.row] objectForKey:@"Flag"]);
+
+        
+    });
+    return cell;
+}
+
+
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
